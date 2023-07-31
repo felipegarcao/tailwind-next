71ae6605c642ed361a77f24409736eb1d95a879b
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2023-07-31 20:13:39 -0300

    Logo e Input de Busca && Menu de Navegação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Sidebar } from '@/components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="grid min-h-screen grid-cols-app">
-          <aside className="border-r border-zinc-200 px-5 py-8">aside</aside>
+          <Sidebar />
 
           <main className="px-4 pb-12 pt-8">{children}</main>
         </div>
