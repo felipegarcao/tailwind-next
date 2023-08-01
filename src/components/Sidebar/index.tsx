5485dcbaa6fbf9eb9f5508f2942fb9c4160885ce
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2023-07-31 21:02:26 -0300

    Widget de Espaço Usado

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,12 @@
 import { Logo } from './Logo'
-import { Search } from 'lucide-react'
+import { Search, LifeBuoy, Cog } from 'lucide-react'
 import { MainNavigation } from './MainNavigation'
+import { NavItem } from './MainNavigation/NavItem'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
 
 export function Sidebar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-4 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
       <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
@@ -16,6 +18,15 @@ export function Sidebar() {
       </div>
 
       <MainNavigation />
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+
+        <UsedSpaceWidget />
+      </div>
     </aside>
   )
 }
