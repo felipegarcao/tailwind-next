869fdbc4bdf04e2775af0ae73af43c6e6276e645
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2023-08-01 15:09:45 -0300

    Upload do Avatar

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,16 @@ module.exports = {
         profile: 'max-content 1fr max-content',
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
-      colors: {},
+
+      borderWidth: {
+        6: '6px',
+      },
+
+      colors: {
+        violet: {
+          25: '#fcfaff',
+        },
+      },
     },
   },
   plugins: [],
