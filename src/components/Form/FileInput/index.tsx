6921d2afa6d045b02adfda16eee782a1324050fd
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2023-08-01 15:37:26 -0300

    Componentizando File Input

---
+++ src/components/Form/FileInput/index.tsx
@@ -0,0 +1,7 @@
+import { Control } from './Control'
+import { ImagePreview } from './ImagePreview'
+import { Root } from './Root'
+import { Trigger } from './Trigger'
+
+export { Control, ImagePreview, Root, Trigger }
+
