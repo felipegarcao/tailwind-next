dcc0f14b67ea1452be26ba70ccf93b0bb3da735a
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2023-08-01 16:13:06 -0300

    Context e Preview da Imagem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -149,7 +149,7 @@ export default function Home() {
             </label>
             <FileInput.Root>
               <FileInput.Trigger />
-              <FileInput.Control />
+              <FileInput.Control multiple />
             </FileInput.Root>
           </div>
 
