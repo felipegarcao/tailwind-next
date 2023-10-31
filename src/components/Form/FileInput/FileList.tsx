0c42505287d450da3bc0bcebcaae3c2121abff77
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2023-10-31 10:43:37 -0300

    Criando componente FileItem

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { formatBytes } from '@/utils/format-bytes'
-import { Trash2, UploadCloud } from 'lucide-react'
+
+
+import { FileItem } from './FileItem'
 import { useFileInput } from './Root'
-import { Button } from '@/components/Button'
 
 export function FileList() {
   const { files } = useFileInput()
@@ -12,39 +12,7 @@ export function FileList() {
     <div className="mt-4 flex flex-col gap-3">
       {files.map((file) => {
         return (
-          <div
-            key={file.name}
-            className="group flex items-start gap-4 rounded-lg border border-zinc-200 p-4"
-          >
-            <div className="rounded-full border-4 border-violet-100 bg-violet-200 p-2 text-violet-600">
-              <UploadCloud className="h-4 w-4" />
-            </div>
-
-            <div className="flex flex-1 flex-col items-start gap-1">
-              <div className="flex flex-col">
-                <span className="text-sm font-medium text-zinc-700">
-                  {file.name}
-                </span>
-                <span className="text-sm text-zinc-500">
-                  {formatBytes(file.size)}
-                </span>
-              </div>
-
-              <div className="flex w-full items-center gap-1">
-                <div className="h-2 flex-1 rounded-full bg-zinc-100">
-                  <div className="h-2 w-4/5 rounded-full bg-violet-600" />
-                </div>
-                <span className="text-sm font-medium text-zinc-700">80%</span>
-              </div>
-            </div>
-
-            <Button
-              type="button"
-              variant='ghost'
-            >
-              <Trash2 className="h-5 w-5 text-zinc-500" />
-            </Button>
-          </div>
+         <FileItem key={file.name} name={file.name} size={file.size} />
         )
       })}
     </div>
